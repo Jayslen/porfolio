383f7de2d5870c53f1036f96ae3b61f6d38ab790
Jayslen <jayslen134@gmail.com>
2026-05-03 17:54:40 -0400

    Update astro and tailwind to latest version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  i18n:{
-    locales: ['en', 'es'],
-    defaultLocale: 'es',
-    routing:{
-      prefixDefaultLocale: false ,
-    }
-  }
+  i18n: {
+    locales: ["en", "es"],
+    defaultLocale: "es",
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
